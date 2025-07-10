e8e4892ed563806331bd128664f2b15d31b22056
DImon1809 <klimovd131@gmail.com>
2025-07-10 22:14:40 +0300

    Updates

--- assets/ChartsPage-ChqCBbOj.js
+++ assets/ChartsPage-ChqCBbOj.js
@@ -1,4 +1,4 @@
-import{r as W,R as U,j as M}from"./index-BWJOOnKs.js";/*!
+import{r as W,R as U,j as M}from"./index-D8d4QwVg.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
